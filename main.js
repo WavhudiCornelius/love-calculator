a85d2a658e0b19f4f2e6a08bd1dffa9b6076c119
Cornelius Wavhudi Mulaudzi <corneliusmulaudzi@gmail.com>
2020-07-22 06:12:01 +0200

    deploying my first static site on heruko

---
+++ main.js
@@ -0,0 +1,48 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+
+function myfunction () {
+
+    var fname = document.getElementById("fname").value;
+    var sname = document.getElementById("sname").value;
+
+    // fetching the data from the love calculator API
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+            "x-rapidapi-key": "f4dbd0a238mshc586ba8b784bcc0p170adfjsnea3374ade98d"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        document.getElementById("prc").innerHTML = response['percentage'];
+        document.getElementById("msg").innerHTML = response['result'];
+    });
+}
+
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault()
+});
